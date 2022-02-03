5ba2726cc607615ff0b5bd41d496a0efc9062182
anshuman <anshuman011994@gmail.com>
2022-02-03 20:05:33 +0530

    added getState

--- src/App.js
+++ src/App.js
@@ -6,12 +6,37 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+//store- stores data, think of state
+//reducer-fucntion that used to update store
+//reducer takes two arguments, state and action
+//state-old state/state before update
+//action-what happend/what update
+//return updated or old state
+
+import { createStore } from 'redux'
+
+//intial store or state
+const initialStore = {
+  count: 0
+}
+
+//store.getState();  returns the latest state
+
+//reducer
+const reducer = (state, action) => {
+  console.log({ state, action });
+  return state;
+}
+
+const store = createStore(reducer, initialStore)
+console.log(store.getState());
+
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
